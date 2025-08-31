a74477810ecaffd289559c30db6856c53b7f3466
Weiwei <512403963@qq.com>
2025-08-31 12:42:27 +0800

    启用GAS和AngelscriptGAS插件

--- Source/Angelscript_Aura/Angelscript_Aura.Build.cs
+++ Source/Angelscript_Aura/Angelscript_Aura.Build.cs
@@ -10,7 +10,7 @@ public class Angelscript_Aura : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "AngelscriptGAS" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
